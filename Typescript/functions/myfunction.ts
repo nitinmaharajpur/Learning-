e2b9809690ae608ea9915a8036ba1c5f2bf14b90
Nitin Kumar <nitimaharajpur@gmail.com>
2024-07-15 00:19:09 +0530

    functions and their types

--- Typescript/functions/myfunction.ts
+++ Typescript/functions/myfunction.ts
@@ -8,7 +8,16 @@ function getUpper(val:string){
 addTwo(5)
 getUpper("nitiN") 
 
-let login= (name:string,email:string,isPaid:boolean)=>{
+// let login= (name:string,email:string,isPaid:boolean)=>{
     
+// }
+
+function f(num:string  ):string{
+    // return 2;that creates problem thats why we use this :string to give that what we return
+    return"OK";
+}
+const fun=(str:string):string=>{
+    // return 2;that creates problem thats why we use this :string to give that what we return
+    return "ok";
 }
 export {}
