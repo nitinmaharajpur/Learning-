91b1bd10d313fbd5b15ab2d8c1b28f7239e4c77b
Nitin Kumar <nitimaharajpur@gmail.com>
2024-07-16 23:03:18 +0530

    Learning Objects in typescript

--- Typescript/functions/myfunction.ts
+++ Typescript/functions/myfunction.ts
@@ -20,4 +20,18 @@ const fun=(str:string):string=>{
     // return 2;that creates problem thats why we use this :string to give that what we return
     return "ok";
 }
+
+const heros=["thor","spiderman","ironman"]
+
+heros.map(hero=>{
+    return `hero is ${hero}`
+})
+
+function handleError(errmsg:string):void{
+    console.log(errmsg);
+}
+
+function fail(msg: string): never {
+    throw new Error(msg);
+  }
 export {}
---
+++ Typescript/object/myObject.ts
@@ -0,0 +1,9 @@
+const User={
+    name:"Nitin Kumar",
+    email:"nitinmaharajpur@gmail.com",
+    isActive:true
+}
+
+function createUser({name }){
+
+}
