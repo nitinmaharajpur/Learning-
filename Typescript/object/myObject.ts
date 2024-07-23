f36eb2fbe4520040ea69c4cb51ed6d4b864ad12f
Nitin Kumar <nitimaharajpur@gmail.com>
2024-07-23 11:45:31 +0530

    Type aliases

--- Typescript/object/myObject.ts
+++ Typescript/object/myObject.ts
@@ -4,6 +4,19 @@ const User={
     isActive:true
 }
 
-function createUser({name }){
 
+// weird behaviour of typescript
+function createUser({name :string,isActive:bool}){}
+
+// createUser({name:"nitin",isActive:false})
+//if we add more values then it gives error
+// createUser({name:"nitin",isActive:false,email:"nitinmaharajpur@gmail.com"})
+// if we use a variable to store and then pass it willnot give any type of error
+let m={name:"nitin",isActive:false,email:"nitinmaharajpur@gmail.com"}
+createUser(m);//so this a weird situation of ts
+
+//how we return a an object function
+function create():{}{
+    return {};
 }
+
---
+++ Typescript/type_aliases/readOnlyAndoptional.ts
@@ -0,0 +1,21 @@
+//Define type aliases
+type user={
+    readonly _id:string,
+    name:string,
+    email:string,
+    isAva:boolean,
+    card?:string//optional mode need not to redefine
+}
+//now store values on a variable
+
+let myUser:user={
+    _id:"1234",
+    name:"nitin",
+    email:"n@gmail.com",
+    isAva:true,
+    
+}
+
+myUser.name="Tanvi"
+//cannot redefine value to because it is in readmode only
+// myUser._id="11"
